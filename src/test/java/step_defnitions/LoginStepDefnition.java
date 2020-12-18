42ef4b64c953c24e550a57e339453bb3f9ad377e
Ebrish <66562433+ebrish3000@users.noreply.github.com>
2020-12-18 14:37:10 -0800

    firstcommit

---
+++ src/test/java/step_defnitions/LoginStepDefnition.java
@@ -0,0 +1,24 @@
+package step_defnitions;
+
+import io.cucumber.java.en.*;
+
+public class LoginStepDefnition {
+    @Given("user is on the landing page")
+    public void user_is_on_the_landing_page() {
+
+    }
+
+
+    @When("user enters credentials")
+    public void user_enters_credentials() {
+
+    }
+    @When("user clicks login button")
+    public void user_clicks_login_button() {
+
+    }
+    @Then("user supposed to see welcome message")
+    public void user_supposed_to_see_welcome_message() {
+
+    }
+}
